5270112aa8c1e00121c1f39cfcaf122f29ed6800
Nicholas Brown <nicholas.hugh.brown@gmail.com>
2022-12-21 19:20:01 +0000

    added more tests for FilmActorID Class

--- src/test/java/UIElements/ProjTSI/FilmActorIdTest.java
+++ src/test/java/UIElements/ProjTSI/FilmActorIdTest.java
@@ -6,15 +6,28 @@ import org.junit.jupiter.api.Test;
 public class FilmActorIdTest {
 
     @Test
-    public void testConstructor() {
+    public void testEmptyConstructor() {
         FilmActorId testFilmActorId = new FilmActorId(44, 55);
         Assertions.assertNotNull(testFilmActorId);
     }
 
     @Test
-    public void testGetters() {
+    public void testConstructor() {
         FilmActorId testFilmActorId = new FilmActorId(44, 55);
         Assertions.assertEquals(44, testFilmActorId.getFilmid());
         Assertions.assertEquals(55, testFilmActorId.getActorid());
     }
+
+    @Test
+    public void testEquals() {
+        FilmActorId testFilmActorId1 = new FilmActorId(44, 55);
+        FilmActorId testFilmActorId2 = new FilmActorId(44, 66);
+        FilmActorId testFilmActorId3 = new FilmActorId(33, 55);
+        FilmActorId testFilmActorId4 = new FilmActorId(33, 66);
+        Assertions.assertTrue(testFilmActorId1 == testFilmActorId1);
+        Assertions.assertFalse(testFilmActorId1 == testFilmActorId2);
+        Assertions.assertFalse(testFilmActorId1 == testFilmActorId3);
+        Assertions.assertFalse(testFilmActorId1 == testFilmActorId4);
+    }
+
 }

7bbbda13ee2136664833bf9b09f75e616b815969
Nicholas Brown <nicholas.hugh.brown@gmail.com>
2022-12-21 17:31:02 +0000

    added tests for FilmActor Class

--- src/test/java/UIElements/ProjTSI/FilmActorTest.java
+++ src/test/java/UIElements/ProjTSI/FilmActorTest.java
@@ -1,6 +1,6 @@
 package UIElements.ProjTSI;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class FilmActorTest {
@@ -8,6 +8,20 @@ public class FilmActorTest {
     public void testConstructor(){
         FilmActor testFilmActor = new FilmActor();
         Assertions.assertNotNull(testFilmActor);
+    }
+
+    @Test
+    public void testGettersSetters() {
+        FilmActor testFilmActor = new FilmActor();
+        Actor testActor = new Actor("George", "Bloyggs");
+        testActor.setActorid(15);
+        Film testFilm = new Film("AGoodFilm",29);
+
+        testFilmActor.setActor(testActor);
+        testFilmActor.setFilm(testFilm);
+
+        Assertions.assertEquals(15, testFilmActor.getActor().getActorid());
+        Assertions.assertEquals(29, testFilmActor.getFilm().getFilmid());
     }
 
 }

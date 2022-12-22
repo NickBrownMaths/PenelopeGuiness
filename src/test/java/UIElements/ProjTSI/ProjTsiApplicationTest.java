2f4390b7f5e0a935f7768f4b6b56d1ab073be2e4
Nicholas Brown <nicholas.hugh.brown@gmail.com>
2022-12-22 08:28:20 +0000

    testing

--- src/test/java/UIElements/ProjTSI/ProjTsiApplicationTest.java
+++ src/test/java/UIElements/ProjTSI/ProjTsiApplicationTest.java
@@ -2,18 +2,25 @@ package UIElements.ProjTSI;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 
 public class ProjTsiApplicationTest extends ProjTsiApplication {
 
-  @Test
-  public void testMain() {
-      ProjTsiApplication App = new ProjTsiApplication();
-      Assertions.assertNotNull(App);
-  }
+    @Mock
+    private ActorRepository testAR;
 
+    @Test
+    public void testDefaultConstructor() {
+        ProjTsiApplication App = new ProjTsiApplication();
+        Assertions.assertNotNull(App);
+    }
 
+    public void testConstructor() {
+        ProjTsiApplication App = new ProjTsiApplication(testAR);
+        Assertions.assertNotNull(App);
+    }
 
 }
